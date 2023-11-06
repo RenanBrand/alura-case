ea465cd81550a32e50b20dd9fbced122a6821b6b
Renan Brandão <renanbrandmelo@gmail.com>
2023-11-06 20:33:49 -0300

    init commit

---
+++ pages/404.js
@@ -0,0 +1,18 @@
+import Link from "../src/components/Link";
+
+export default function Page404() {
+
+    return (
+        <div>
+            <h1>
+                Você se perdeu e caiu na página 404 :O 
+            </h1>
+            <Link href="/">
+            
+            Ir para a página do Home
+
+            </Link>
+            
+        </div>
+    )
+}
---
+++ pages/index.js
@@ -0,0 +1,36 @@
+import React from "react";
+import Link from "../src/components/Link";
+import HomeScreen from '../src/components/screens/homeScreen';
+
+export default HomeScreen;
+// function Title({ children, as }) {
+//     const Tag = as;
+//     return (
+//         <React.Fragment>
+//             <Tag>
+//                 {children}
+//             </Tag>
+//             <style jsx>{`
+//         ${Tag} {
+//         color: red;
+//         }
+//         `}</style>
+//         </React.Fragment>
+
+//     );
+// }
+
+// export default function HomePage() {
+
+//     return (
+//         <div>
+//             <Title as="h2">Alura Cases - home</Title>
+//             <Link href="/faq">
+
+//                 Ir para página do FAQ
+
+//             </Link>
+//             <Footer />
+//         </div>
+//     )
+// }
